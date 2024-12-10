ccce87a4b285d454654cb2bc587497e3d7ce4fee
nicolas <nprenet@gmail.com>
2024-12-10 14:42:58 +0100

    Abbreviation expansion keep mask information in the resulting XML.

--- curation_tools_and_files/bin/expand_pagexml.py
+++ curation_tools_and_files/bin/expand_pagexml.py
@@ -1,33 +1,82 @@
 #!/usr/bin/env python3
 
+"""
+This script is intended for use on **abbreviated** transcriptions:
+
+- expand the abbreviations
+- replace abbreviation annotations with expansion annotations (offset, length, <original string>)
+- very basic clean-up, that does not affect the said offsets
+"""
+
 import xml.etree.ElementTree as ET
 from pathlib import Path
 import sys
 import re
+from typing import List,Tuple
 
 
 USAGE=f"USAGE: {sys.argv[0]} <source page xml> <target page xml> ]"
 
 def clean_up( transcr: str ) -> str:
-    transcr = transcr.replace('&#13','')
-    transcr = transcr.replace('\u200b','')
-    transcr = transcr.replace('\uf2f7','')
+    #transcr = transcr.replace('&#13','') # not needed for line transcriptions
+    transcr = re.sub(r'[\u00a0\u2000-\u200b\u202f]',' ',transcr) # thin space
+    
+    # koenigsfelden transcriber forgot some of those
+    # in the text -> character-count keeping filtering
     transcr = transcr.replace(u'ꝫc̄','etc')
     transcr = transcr.replace(u'','Ü')
-    transcr = re.sub(r'  +',' ', transcr)
     return transcr
 
 
-def expand( txt, abbreviations):
-    curr, new = 0, ''
-    if not abbreviations or txt is None:
-        return ''
-    for offset,length,exp in abbreviations:
-        offset, length = int(offset), int(length)
-        new += (txt[curr:offset] + exp )
-        curr = offset + length
-    return new + txt[curr:]
+def expand( txt, masks, debug=False):
+    """
+    From a list of tuples (offset, length, <expanded text>), expand
+    the abbreviation contained in the input text.
+
+    Args:
+        txt (str): the abbreviated text.
+        masks (Tuple[int,int,str]): a list of tuples (offset, length, <expanded text>).
+        debug (bool): return expanded bits like t<hi>s
+    Returns:
+        Tuple[str,List[Tuple[int,int,str]]]: A triplet with the expanded text and a list of tuples
+            storing the new (offset, length) information for the expanded part, relative
+            to the output string, as well as the original, abbreviation string.
+    """
+    input_cursor, new = 0, ''
+    output_masks = []
+    output_offset = 0
+    if txt is None:
+        return ('',[])
+    if not masks:
+        return (txt,[])
+    for abb_offset,abb_length,abb_exp in masks:
+        abb_offset, abb_length = int(abb_offset), int(abb_length)
+        new += txt[input_cursor:abb_offset] + (f'<{abb_exp}>' if debug else abb_exp )
+        input_cursor = abb_offset + abb_length
+        output_masks.append((abb_offset+output_offset,len(abb_exp), txt[abb_offset:abb_offset+abb_length]))
+        output_offset += len(abb_exp)-abb_length
+    return (new + txt[input_cursor:], output_masks)
+
     
+def rebuild_custom_annotations( annotations: str, masks:List[Tuple[int,int,str]]):
+    """
+    Given XML annotation string of the kind:
+    
+    ```xml
+    readingOrder {index:0;} person {offset:0; length:62;ref:per000089;} abbrev {offset:29; length:1;expansion:et;} \
+    abbrev {offset:56; length:2;expansion:gra;} abbrev {offset:66; length:4;expansion:omni;} ... 
+    ```
+    Replace abbreviation masks with provided list; annotations that contain an offset
+    -such as 'person'-are removed (they become obsolete); other annotations are kept. 
+    
+    """
+    if not masks:
+        return annotations
+    pairs = re.findall(r'(\w+) *{([^}]+)}', annotations)
+    if pairs:
+        pairs = [ p for p in pairs if p[0]!='abbrev' and p[1].find('offset')<0 ]
+        pairs += [ ('expansion', f'offset:{m[0]}; length:{m[1]}; abbrev:{m[2]};') for m in masks ]
+    return ' '.join([ f'{name} {{{value}}}' for (name,value) in pairs ])
 
 if __name__ == "__main__":
 
@@ -41,7 +90,7 @@ if __name__ == "__main__":
 
 
     ns = { 'pc': "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15" }
-    ET.register_namespace('', ns['pc']
+    ET.register_namespace('', ns['pc'] )
 
     with open( page_path, 'r') as page:
         print(page_path)
@@ -54,12 +103,22 @@ if __name__ == "__main__":
             region_text = []
             for textLineElt in textRegionElt.findall('./pc:TextLine', ns):
 
-                abbreviations = re.findall(r'abbrev *{ *offset:(\d+); *length:(\d+); *expansion:([^;]+); *}', textLineElt.get('custom'))
+                abb_masks = re.findall(r'abbrev *{ *offset:(\d+); *length:(\d+); *expansion:([^;]+); *}', textLineElt.get('custom')) if 'custom' in textLineElt.keys() else []
+    
                 txtLineText = textLineElt.find('./pc:TextEquiv/pc:Unicode', ns)
                 if txtLineText is not None and txtLineText.text is not None:
-                    transcription = clean_up( expand( txtLineText.text, abbreviations ))
+
+                    text_new, abb_masks_new = expand( txtLineText.text, abb_masks )
+                    
+                    transcription = clean_up( text_new )
+                    # clean-up should not mess with the offsets!
+                    assert len(transcription)==len(text_new)
                     txtLineText.text = transcription
                     region_text.append( transcription )
+                    
+                    if 'custom' in textLineElt.keys() and abb_masks_new != []:
+                        textLineElt.set('custom',rebuild_custom_annotations(textLineElt.get('custom'), abb_masks_new))
+
 
             region_text_unicode = textRegionElt.find('./pc:TextEquiv/pc:Unicode', ns)
             if region_text_unicode is not None:
